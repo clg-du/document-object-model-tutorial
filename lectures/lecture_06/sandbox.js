9763ff29ed11fcd440553e3e15fd62040bb2491a
Charlie Lindgren <clg@du.se>
2024-09-25 22:40:29 +0200

    chore: Update lecture_06 index.html and sandbox.js
    - Remove unnecessary line in index.html
    - Remove unused script source in index.html
    - Add Bootstrap classes to elements in sandbox.js
    - Update lecture_06 sandbox.js with code for traversing the DOM
    - Update lecture_07 index.html and sandbox.js with Bootstrap classes

--- lectures/lecture_06/sandbox.js
+++ lectures/lecture_06/sandbox.js
@@ -1,22 +1,72 @@
 
-// Lecture 6 - Adding Bootstrap classes to elements
+// Lecture 6 - Traversing the DOM
 
-// We source the changes from the previous lecture in the HTML document by
-// adding the script. Therefore, those changes are already implemented here.
-
-// We now want to add some Bootstrap classes to the elements in the HTML
-// document, so that the page looks better.
+// Traversing the DOM is the process of navigating through the structure of a
+// document. This can be done using the properties and methods of the DOM API.
+// Here are some examples of how we can traverse the DOM using JavaScript:
 
 // ---------------------------------------------------
 
-// Adding some base Bootstrap classes to the elements in the HTML document
+// Selecting the parent element of an element
 
-// Select the h1 element on the page and add the Bootstrap class display-4 and
-// center it on the page by adding the Bootstrap class text-center. We also add
-// some margin to the top and bottom of the element by adding the Bootstrap
-// class mb-4 and mt-4.
-document.querySelector('h1').classList.add('display-4', 'text-center', 'mb-4', 'mt-4');
+// Here is an example of how we can select the parent element of an element using
+// the parentNode property:
 
-// Let's enclose the div tags within the 
+// Select the h2 element with the id of 'first-header'
+const firstHeader = document.getElementById('first-header');
 
+// Select the parent element of the h2 element
+const section = firstHeader.parentNode;
+
+// Select the parent element of the section element
+const main = section.parentNode;
+
+// ---------------------------------------------------
+
+// Selecting the child elements of an element
+
+// Here is an example of how we can select the child elements of an element using
+// the children property:
+
+// Select the div with id 'dom-list' element and store it in a variable
+const domList = document.getElementById('dom-list');
+
+// Select the child elements of the div element
+const listItems = domList.children;
+
+// Select the first child element of the div element
+// This is the h2 element.
+const firstChild = domList.firstElementChild;
+
+// Select the last child element of the div element
+// This is the ul element, including all of its children.
+const lastChild = domList.lastElementChild;
+
+// Select the nth child element of the ul list
+const secondListItem = lastChild.children[1];
+const thirdListItem = lastChild.children[2];
+
+// ---------------------------------------------------
+
+// Selecting the sibling elements of an element
+
+// Here is an example of how we can select the sibling elements of an element using
+// the nextSibling and previousSibling properties:
+
+// Select the h2 element with the id of 'first-header'
+const header = document.getElementById('first-header');
+
+// Select the next sibling element of the h2 element
+// This is the whitespace between the h2 and p elements.
+const whitespace = header.nextSibling;
+
+// Select the next sibling element of the whitespace element
+// This is the p element.
+const paragraph = whitespace.nextSibling;
+
+// Select the previous sibling element of the p element
+// This is the h2 element.
+const previousHeader = paragraph.previousSibling;
+
+// ---------------------------------------------------
 
